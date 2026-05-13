630d8f5e69c9a20a3a976754471a0b8df4081eed
Stuart <199536630+Stuart428@users.noreply.github.com>
2026-05-13 12:21:52 +1000

    awdawdwa

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const moveNoButton = () => {
 }
 twofadiv.style.display = 'none'; 
 let currenttwofacode = 615819;
-document.querySelector('#twofaCode').innerHtml = `Your 2fa code: ${currenttwofacode}`;
+twofaCodeElement.innerHTML = `Your 2fa code: ${currenttwofacode}`;
 window.setInterval(function(){
   randomtwofacode();
 }, 2000);
@@ -20,7 +20,7 @@ window.setInterval(function(){
 function randomtwofacode()
 {
   currenttwofacode = Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
-  document.querySelector('#twofaCode').innerHtml = `Your 2fa code: ${currenttwofacode}`;
+  twofaCodeElement.querySelector('#twofaCode').innerHTML = `Your 2fa code: ${currenttwofacode}`;
 }
 
 //product.addEventListener('click', moveNoButton);
